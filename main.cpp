7a9ecf15c497800620e4e720e9dd651a0fe586b2
MaraOgn <121185268+MaraOgn@users.noreply.github.com>
2023-05-08 19:15:14 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,85 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <ctime>
+
 
 using namespace std;
 
-int main()
-{
-    string filename, text;
+// Функција која го црта вратилото на основа на бројот на погрешни обиди
+void drawHangman(int wrongAttempts) {
+    vector<string> hangmanDrawing = {
+        "  ____",
+        "  |  |",
+        "  |  O",
+        "  | /|\\",
+        "  | / \\",
+        "__|______"
+    };
 
-    cout << "Vnesi go imeto na fajlot: ";
-    cin >> filename;
-
-    cout << "Vnesi tekst: ";
-    cin.ignore();
-    getline(cin, text);
-
-    ofstream outfile;
-    outfile.open(filename, ios::app);
-
-    if (outfile.is_open())
-    {
-        outfile << text << endl;
-        outfile.close();
-        cout << "Tekstot e zapisan vo fajlot" << endl;
+    for (int i = 0; i < wrongAttempts; i++) {
+        cout << hangmanDrawing[i] << endl;
     }
-    else
-    {
-        cout << "Ne moze da se otvori fajlot" << endl;
+}
+
+// Функција која ги проверува погодоците и го враќа променетиот збор
+string checkGuess(const string& word, string& guessedWord, char guess) {
+    for (int i = 0; i < word.length(); i++) {
+        if (word[i] == guess) {
+            guessedWord[i] = guess;
+        }
+    }
+    return guessedWord;
+}
+
+int main() {
+    ifstream file("words.txt");  // Датотека со зборови
+    if (!file) {
+        cout << "Ne moze da se otvori fajlot so zborovi" << endl;
+        return 1;
+    }
+
+    vector<string> words;
+    string word;
+    while (file >> word) {
+        words.push_back(word);
+    }
+    file.close();
+
+    srand(time(0));
+    int randomIndex = rand() % words.size();
+    string selectedWord = words[randomIndex];
+    string guessedWord(selectedWord.length(), '_');
+
+    int maxAttempts = 6;  // Максимален број на обиди
+    int wrongAttempts = 0;
+
+    while (wrongAttempts < maxAttempts) {
+        cout << "Pogodi go zborot: " << guessedWord << endl;
+
+        char guess;
+        cout << "Vnesi bukva: ";
+        cin >> guess;
+
+        string updatedWord = checkGuess(selectedWord, guessedWord, guess);
+        if (updatedWord == guessedWord) {
+            wrongAttempts++;
+            cout << "Seuste ne go pogodi zborot,preostanati obidi: " << maxAttempts - wrongAttempts << endl;
+        } else {
+            guessedWord = updatedWord;
+        }
+
+        drawHangman(wrongAttempts);
+
+        if (guessedWord == selectedWord) {
+            cout << "Cestitki! Go pogodi zborot: " << selectedWord << endl;
+            break;
+        }
+    }
+
+    if (wrongAttempts >= maxAttempts) {
+        cout << "Izgubi! Zborot e : " << selectedWord << endl;
     }
 
     return 0;
